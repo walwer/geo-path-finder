849ec1689489a3e30d0fcef7116cfd0bc734a849
Bartek Waliszko <bartekwaliszko@gmail.com>
2020-11-04 22:52:33 +0100

    added core module with mock input

---
+++ core/PathFinder/pathFinder.js
@@ -0,0 +1,27 @@
+const Calculator = require('../../model/DistanceCalculator/distanceCalculator');
+
+class PathFinder {
+    constructor() {
+        this.waypoints = [];
+        this.calulator = new Calculator();
+    }
+
+    loadWaypoints(waypoints) {
+        this.waypoints = waypoints;
+    }
+
+    generatePath() {
+        for(let point in this.waypoints) {
+            let bestMatching = null;
+            this.calulator.setStartingPoint(point.lat, point.lon);
+
+            for(let matching in this.waypoints) {
+                this.calulator.setFinishPoint(matching.lat, point.lon);
+
+                let distance = this.calulator.getDistance();
+                if(distance < bestMatching) bestMatching = matching;
+            }
+        }
+    }
+
+}
