46549e0edb43f81e95c2ea93440f97740305feff
Bartek Waliszko <wwalwer1337@gmail.com>
2020-11-09 19:33:48 +0100

    empty calculation logic

--- model/PathFinder/pathFinder.js
+++ model/PathFinder/pathFinder.js
@@ -6,6 +6,7 @@ class PathFinder {
         this.waypoints = [];
         this.calulator = new Calculator();
         this.validator = new Validator();
+        this.path = [];
     }
 
     loadWaypoints(waypoints) {
@@ -21,51 +22,12 @@ class PathFinder {
         this.validator.validate(data);
     }
 
-    findNearestWaypoint(startingPoint, path, availableItems, currentIteration) {
-        if (availableItems.length === 0 || availableItems === []) return path;
+    findNearestWaypoint() {
 
-        currentIteration++;
-
-        this.calulator.setStartingPoint(startingPoint.latitude, startingPoint.longitude);
-        let bestMatching = null;
-
-        for (let matching in availableItems) {
-            if (availableItems[matching] === path[path.length - 1]) continue;
-
-            this.calulator.setFinishPoint(availableItems[matching].latitude, availableItems[matching].longitude);
-            let distance = this.calulator.getDistance();
-
-            if (distance === 0) {
-                continue;
-            }
-
-            if (distance < bestMatching || bestMatching === null) {
-                bestMatching = matching;
-            }
-        }
-
-        if (bestMatching) {
-            console.log(`Matched the step for ${startingPoint.latitude} a ${availableItems[bestMatching].latitude}`);
-            path.push[availableItems[bestMatching]];
-            availableItems.splice(bestMatching, 1);
-        }
-
-        return this.findNearestWaypoint(path[path.length - 1], path, availableItems, currentIteration);
     }
 
     generatePath() {
-        let tempWaypointsOrder = JSON.parse(JSON.stringify(this.waypoints));
-        let startingPointIndex = 0;
-        let finalPathOrder = [];
 
-        let startingPoint = tempWaypointsOrder[startingPointIndex];
-
-        finalPathOrder.push(tempWaypointsOrder[startingPointIndex]);
-        tempWaypointsOrder.splice(startingPointIndex, 1);
-
-        let path = (this.findNearestWaypoint(startingPoint, finalPathOrder, tempWaypointsOrder, 0));
-
-        console.log(path);
     }
 }
 
