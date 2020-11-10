91d27cecac15b29b58ab5c78e7f470076cb70190
Bartek Waliszko <wwalwer1337@gmail.com>
2020-11-10 23:31:33 +0100

    added map generator

--- model/PathFinder/pathFinder.js
+++ model/PathFinder/pathFinder.js
@@ -26,8 +26,42 @@ class PathFinder {
 
     }
 
-    generatePath() {
 
+    generateDistanceMap() {
+        for (let point of this.waypoints) {
+            point['dependencies'] = [];
+
+            for (let target in this.waypoints) {
+                this.calulator.setStartingPoint(point.latitude, point.longitude);
+                this.calulator.setFinishPoint(this.waypoints[target].latitude, this.waypoints[target].longitude);
+                const distance = this.calulator.getDistance();
+
+                if (distance > 0) {
+                    point.dependencies.push({
+                        index: target,
+                        distance,
+                    });
+                }
+            }
+        }
+    }
+
+    generatePath() {
+        this.generateDistanceMap();
+        let path = [];
+        let maxLength = this.waypoints.length;
+        // console.log(this.waypoints);
+        // path.push(this.waypoints[0]); //Set starting point here
+        //
+        // while (path.length <= maxLength) {
+        //     let lastPointInPath = path[path.length - 1];
+        //     lastPointInPath.dependencies.sort((a, b) => a.distance - b.distance);
+        //     path.push(this.waypoints[lastPointInPath.dependencies[0].index]);
+        //     this.waypoints.splice(lastPointInPath.dependencies[0].index, 1);
+        // }
+        //
+        // console.log(path);
+        return path;
     }
 }
 
